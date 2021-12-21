28f4d80c775bd2f8c37b34943fbedf7694cd4c5a
DuyguYLMZ <duyguyilmaz032@gmail.com>
2021-12-21 08:55:03 +0300

    Info added

---
+++ lib/model/announcement.dart
@@ -0,0 +1,42 @@
+import 'package:dao/model/info.dart';
+
+
+class Announcement {
+  List<Info> infoList;
+  String announcementDate;
+
+  Announcement(this.infoList, this.announcementDate);
+/*
+
+  String get announcementDate => _announcementDate;
+  List<Info> get infoList => _infoList;
+
+  Announcement({
+    List<Info> infoList,
+    String announcementDate}){
+    _infoList = infoList;
+    _announcementDate = announcementDate;
+  }
+
+  Announcement.fromJson(Map<String, dynamic> json) {
+    _announcementDate = json["announcementDate"];
+    if (json["infoList"] != null) {
+      _infoList = [];
+      json["infoList"].forEach((v) {
+        _infoList.add(Info.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    var map = <String, dynamic>{};
+    map["announcementDate"] = _announcementDate;
+    if (_infoList != null) {
+      map["infoList"] = _infoList.map((v) => v.toJson()).toList();
+    }
+    return map;
+  }
+
+}
+*/
+}
